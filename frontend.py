12434bf4485885ae6edda4671fd8a94005b1d5c5
Aditya Gading Saputra <163370158+Rusaven@users.noreply.github.com>
2024-07-17 15:45:04 +0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,27 +1,27 @@
 import streamlit as st
 import requests
-import time
 
-def get_data():
-    try:
-        response = requests.get('http://127.0.0.1:5000/data')  # Adjust URL to your backend endpoint
-        if response.status_code == 200:
-            return response.json()['data']
-        else:
-            st.error(f'Error: {response.status_code}')
-            return None
-    except requests.exceptions.RequestException as e:
-        st.error(f'Error: {e}')
+def fetch_dummy_data():
+    url = 'https://127.0.0.1:5000/get_dummy_data'  # Ganti dengan URL backend Anda
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
         return None
 
 def main():
-    st.title('Real-time Data Display')
-    
-    while True:
-        data = get_data()
-        if data:
-            st.write('Received Data:', data)
-        time.sleep(5)  # Adjust polling interval as needed
+    st.title('Dummy Data Display')
+
+    # Fetch dummy data from backend
+    dummy_data = fetch_dummy_data()
+
+    if dummy_data:
+        st.write('Temperature:', dummy_data['temperature'])
+        st.write('Humidity:', dummy_data['humidity'])
+        st.write('Pressure:', dummy_data['pressure'])
+        st.write('Status:', dummy_data['status'])
+    else:
+        st.write('Failed to fetch data from backend.')
 
 if __name__ == '__main__':
     main()
