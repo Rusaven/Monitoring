05b8ff8c521c39f1effad224666b4b3d627003c9
Aditya Gading Saputra <163370158+Rusaven@users.noreply.github.com>
2024-07-17 16:11:22 +0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,28 +1,24 @@
-# app.py
+# frontend.py
+
 import streamlit as st
 import requests
 
-def fetch_dummy_data():
-    url = 'http://10.170.6.136:5000/get_dummy_data'  # Ganti dengan URL backend Anda
+# Fungsi untuk mengambil data dari backend
+def fetch_data():
+    url = 'http://localhost:5000/data'  # Ganti dengan URL backend yang sesuai
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        return None
+        st.error(f'Error: {response.status_code}')
 
+# Halaman utama Streamlit
 def main():
-    st.title('Dummy Data Display')
+    st.title('Monitoring Website')
 
-    # Fetch dummy data from backend
-    dummy_data = fetch_dummy_data()
-
-    if dummy_data:
-        st.write('Temperature:', dummy_data['temperature'])
-        st.write('Humidity:', dummy_data['humidity'])
-        st.write('Pressure:', dummy_data['pressure'])
-        st.write('Status:', dummy_data['status'])
-    else:
-        st.write('Failed to fetch data from backend.')
+    st.write('Data dari backend:')
+    data = fetch_data()
+    st.write(data)
 
 if __name__ == '__main__':
     main()
